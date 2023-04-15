afab80c304d9e05095eb9d69a3a8e91f2c0443c3
Ahmed Alam El-Deen <ahmed.alameldeen.me@gmail.com>
2023-04-15 22:14:32 +0200

    style: :lipstick: Update button component & CartDropdown styles

--- components/button/button.component.jsx
+++ components/button/button.component.jsx
@@ -2,7 +2,7 @@ import { Button } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
 // style material UI component
-const MyButton = styled(Button)({
+const AddToCartButton = styled(Button)({
   display: "block",
   margin: "0 auto",
   padding: "1rem 2rem",
@@ -16,10 +16,31 @@ const MyButton = styled(Button)({
   },
 });
 
+// make a different material UI button style
+const CheckoutButton = styled(Button)({
+  display: "block",
+  margin: "1rem auto 0",
+  padding: "0.5rem 1rem",
+  backgroundColor: "white",
+  color: "black",
+  border: "2px solid black",
+  fontWeight: "bold",
+  transition: "all 0.3s ease-out",
+  "&:hover": {
+    backgroundColor: "#eee",
+    transform: "scale(1.05)",
+  },
+});
+
 function CenteredButton(props) {
-  //passing the onClick prop to the Button component inside the CenteredButton component
-  const { onClick } = props;
-  return <MyButton onClick={onClick}>Add To Cart</MyButton>;
+  // onClick ---> prop to the Button component inside the CenteredButton component to make the click event available
+  // variant ---> prop to control which design we need to use
+  const { onClick, variant } = props;
+  return variant === "checkout" ? (
+    <CheckoutButton onClick={onClick}>GO TO CHECKOUT</CheckoutButton>
+  ) : (
+    <AddToCartButton onClick={onClick}>ADD TO CART</AddToCartButton>
+  );
 }
 
 export default CenteredButton;
--- components/cart-dropdown/cart-dropdown.component.jsx
+++ components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { CartContext } from "@/contexts/cart.context";
 
 import CartItem from "../cart-item/cart-item.component";
+import CenteredButton from "../button/button.component";
 
 import styles from "./cart-dropdown.module.scss";
 
@@ -12,10 +13,16 @@ const CartDropdown = () => {
   return (
     <div className={styles.container}>
       <div className={styles.items}>
+        {/* Guard-clause to show text in case no products found in cart */}
+        {cartItems.length === 0 && (
+          <span className={styles.text}>Your Cart is Empty</span>
+        )}
         {cartItems.map((item) => {
           return <CartItem key={item.id} cartItem={item} />;
         })}
       </div>
+      {/* Guard-clause to show the checkout button in case products found in cart */}
+      {cartItems.length !== 0 && <CenteredButton variant="checkout" />}
     </div>
   );
 };
